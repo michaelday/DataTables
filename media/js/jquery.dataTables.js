078ea7f3bedc36ff39b029410b59a6fe43cd47b1
Michael Day <manveru.alma@gmail.com>
2013-05-26 20:52:45 -0500

    An imperfect solution...

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -29,14 +29,10 @@
 	"use strict";
 
 	// Define as an AMD module if possible
-	if ( typeof define === 'function' && define.amd )
-	{
-		define( ['jquery'], factory );
-	}
 	/* Define using browser globals otherwise
 	 * Prevent multiple instantiations if the script is loaded twice
 	 */
-	else if ( jQuery && !jQuery.fn.dataTable )
+	if ( jQuery && !jQuery.fn.dataTable )
 	{
 		factory( jQuery );
 	}
